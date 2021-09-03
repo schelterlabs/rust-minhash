3869010c0f45d7877734946ee46395a73729991b
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-09-03 15:39:51 +0200

    Impl from ritchie46/lsh-rs as starting point, now starts the rework. That impl seems to materialize all permutations, python datasketch impl is much better

--- src/bin/minhash.rs
+++ src/bin/minhash.rs
@@ -1,6 +1,9 @@
-use minhash::hello_world;
+use minhash::{MinHash, VecHash};
 
 pub fn main() {
-    let msg = hello_world();
-    println!("{}", msg);
+    let n_projections = 3;
+    let h = <MinHash>::new(n_projections, 5, 0);
+    let hash = h.hash_vec_query(&[1, 0, 1, 0, 1]);
+    assert_eq!(hash.len(), n_projections);
+    println!("{:?}", hash);
 }
